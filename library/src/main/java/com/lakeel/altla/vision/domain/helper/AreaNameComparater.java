40d8db678a2808de8c6c726aac2979f604bed81e
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-03-10 21:04:31 +0900

    [ADD] Implement the view to search areas by Google Place

---
+++ library/src/main/java/com/lakeel/altla/vision/domain/helper/AreaNameComparater.java
@@ -0,0 +1,21 @@
+package com.lakeel.altla.vision.domain.helper;
+
+import com.lakeel.altla.vision.domain.model.Area;
+
+import java.util.Comparator;
+
+public final class AreaNameComparater implements Comparator<Area> {
+
+    public static final AreaNameComparater INSTANCE = new AreaNameComparater();
+
+    private AreaNameComparater() {
+    }
+
+    @Override
+    public int compare(Area o1, Area o2) {
+        if (o1.getName() == null && o2.getName() == null) return 0;
+        if (o1.getName() != null && o2.getName() == null) return 1;
+        if (o1.getName() == null && o2.getName() != null) return -1;
+        return o1.getName().compareTo(o2.getName());
+    }
+}
